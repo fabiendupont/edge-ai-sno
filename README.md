f61094800a574d1db39e1fdefff7ec7af75b6286
Fabien Dupont <fdupont@redhat.com>
2025-05-13 23:11:19 +0200

    More README. Simple transformer
    Signed-off-by: Fabien Dupont <fdupont@redhat.com>

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # Kserve inference for MinIO event
 
 We assume that you have installed OpenShift AI operator.
+We also assume that you have a default storage class for the persistent
+volumes.
 
 ## Configuring the data science cluster
 
 The following command creates a data science cluster configuration that enables
-or disables the features. This file Kserver and ModelMesh for single and multi
-model inference services. The other features are disabled.
+or disables the features. The current configuration enables Kserve and
+ModelMesh for single and multi model inference services. The other features are
+disabled.
 
 ```bash
 oc create -f deploy/datasciencecluster-default-dsc.yaml
@@ -14,27 +17,60 @@ oc create -f deploy/datasciencecluster-default-dsc.yaml
 
 ## Deploying MinIO
 
+MinIO provides an S3-compatible storage backend that is used for the models and
+the files ingested by our predictive model. We start by creating a namespace
+named `minio`.
 
 ```bash
 oc create -f deploy/minio/namespace.yaml
 ```
 
+Then, we create a Secret with the minio credentials that will be used to set
+the root username and password. You should modify the values in your
+environement.
+
+```bash
+oc create -f deploy/minio/secret-minio-credentials.yaml
+```
+
+We also create a ConfigMap with the webhook for the data ingestion bucket. We
+point it to our inference service transformer that will download the file and
+pass it to the predictor.
+
 ```bash
 oc create -f deploy/minio/configmap.yaml
-oc create -f deploy/minio/secret.yaml
 ```
+
+We create the PersistentVolumeClaim to allocate storage for our buckets. For
+simplicity, we use a single volume for all the buckets, but we could split them
+for stronger data isolation.
+
+You should adjust the size to allow storing all your data.
 
 ```bash
 oc create -f deploy/minio/persistentvolumeclaim.yaml
 ```
 
-```bash
-oc create -f deploy/minio/deployment.yaml
-```
+For MinIO to be reachable by other pods with a comprehensive hostname, we
+create services for the API and UI endpoints exposed by the container.
+The service URL for the API is `https://minio.minio.svc:9000`.
+
+The service is annotated with
+`service.beta.openshift.io/serving-cert-secret-name: minio-tls` which triggers
+the creation of a service serving certificate stored in the `minio-tls` secret.
+The key and certificate are used by the pod, so we create the service first.
 
 ```bash
 oc create -f deploy/minio/service.yaml
 ```
+
+Now that all prerequisites have been set up, we deploy the MinIO pod.
+
+```bash
+oc create -f deploy/minio/deployment.yaml
+```
+
+Optionally, we deploy a route to make the services available externally.
 
 ```bash
 oc create -f deploy/minio/route.yaml
--- transformer/simple.py
+++ transformer/simple.py
@@ -20,10 +20,8 @@ import os
 import sys
 from typing import Dict, Union
 
-import numpy
+import numpy as np
 
-from PIL import Image
-from torchvision import transforms
 from kserve import (
     Model,
     ModelServer,
@@ -38,32 +36,13 @@ from kserve.model import PredictorProtocol, PredictorConfig
 from minio import Minio
 import urllib3
 
-def image_transform(model_name, data):
-    """converts the input image of Bytes Array into Tensor
+def get_input_tensor_from_minio(record):
+    """retrieves the numpy array input file from minio and return it
     Args:
-        model_name: The model name
-        data: The input image bytes.
+        record: minio webhook file record
     Returns:
-        numpy.array: Returns the numpy array after the image preprocessing.
+        numpy.array: Returns the numpy array as loaded from the file
     """
-    preprocess = transforms.Compose(
-        [
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ]
-    )
-    if model_name == "mnist" or model_name == "cifar10" or model_name == "yolo11n":
-        preprocess = transforms.Compose(
-            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
-        )
-    byte_array = base64.b64decode(data)
-    image = Image.open(io.BytesIO(byte_array))
-    tensor = preprocess(image).numpy()
-    return tensor
-
-def get_image_b64(record):
     minio_endpoint = os.getenv('MINIO_ENDPOINT')
     minio_username = os.getenv('MINIO_USERNAME')
     minio_password = os.getenv('MINIO_PASSWORD')
@@ -84,18 +63,17 @@ def get_image_b64(record):
     filepath = f"/data/{filename}"
     try:
         minio_object = client.fget_object(minio_bucket_name, filename, filepath)
-        print(f"File retrieved")
     except Exception as e:
         print(f"Failure to get object: {filename}: {e}")
 
-    print(f"Encoding {filepath} to base64")
-    with open(filepath, "rb") as file:
-        image_b64 = base64.b64encode(file.read()).decode("utf-8")
+    input_tensor = np.load(filepath, allow_pickle=True)
+    input_tensor_avg = np.average(input_tensor[:])
+    input_tensor /= input_tensor_avg
 
     print(f"Deleting temporary file {filepath}")
     os.remove(filepath)
 
-    return image_b64
+    return input_tensor
 
 
 class ImageTransformer(Model):
@@ -130,10 +108,10 @@ class ImageTransformer(Model):
             # Input follows the Tensorflow V1 HTTP API for binary values
             # https://www.tensorflow.org/tfx/serving/api_rest#encoding_binary_values
             input_tensors = [
-                image_transform(self.name, get_image_b64(record))
+                get_input_tensor_from_minio(record)
                 for record in payload["Records"]
             ]
-        input_tensors = numpy.asarray(input_tensors)
+        input_tensors = np.asarray(input_tensors)
         infer_inputs = [
             InferInput(
                 name="INPUT__0",
